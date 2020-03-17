9d9dfece69df9e5486fb7fbcb2d6b30363dfa0c6
TimurKTI <56561515+TimurKTI@users.noreply.github.com>
2020-03-18 01:00:17 +0300

    init

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void burger_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            BurgerWindow burgerWindow = new BurgerWindow();
+            
+            burgerWindow.Show();
+            this.Hide();
+        }
+    }
+}
